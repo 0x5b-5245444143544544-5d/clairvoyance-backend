dc52bea3cb50ca1c8ad59e9309899b0e03c7446f
0x5b-5245444143544544-5d <vignesh.natarajan2021@vitstudent.ac.in>
2022-04-25 16:15:48 +0530

    Add baudrate to config

--- config.example.py
+++ config.example.py
@@ -1,1 +1,4 @@
 serial_device="" # your arduino LoRa serial path here /dev/xxx
+baud_rate = 9600 # Serial baud rate, 9600 by default
+packet_prefix="" # optional packet prefix
+packet_suffix="" # optional packet suffix
