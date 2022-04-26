11c6956833fe08435c694146cdc9d6ca75dcb65b
0x5b-5245444143544544-5d <vignesh.natarajan2021@vitstudent.ac.in>
2022-04-26 08:09:50 +0530

    Add MQTT broker port config

--- config.example.py
+++ config.example.py
@@ -1,4 +1,5 @@
 serial_device="" # your arduino LoRa serial path here /dev/xxx
 baud_rate = 9600 # Serial baud rate, 9600 by default
+mqtt_broker_port = 0000 # port of the MQTT broker of choice
 packet_prefix="" # optional packet prefix
 packet_suffix="" # optional packet suffix
