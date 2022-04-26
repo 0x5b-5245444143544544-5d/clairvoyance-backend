3731387f62f4b97216e4ecf60adb7c0125d437d2
0x5b-5245444143544544-5d <vignesh.natarajan2021@vitstudent.ac.in>
2022-04-26 20:37:18 +0530

    Fix string replacements.

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import serial
 import config
 import paho.mqtt.client as mqtt
 
-
-async def read_serial():
-    ser = serial.Serial(config.serial_device, baudrate=config.baud_rate, timeout=1)
-    while True:
-        s = ser.read(10000)
-        print(s.decode("utf-8"))
+client = mqtt.Client()
+client.connect(host="localhost", port=config.mqtt_broker_port)
+ser = serial.Serial(config.serial_device, baudrate=config.baud_rate, timeout=1)
+while True:
+    s = ser.readline().decode()
+    if s and s.startswith(config.packet_prefix) and s.endswith(config.packet_suffix):
+        s = s.replace(config.packet_prefix, "")
+        s = s.replace(config.packet_suffix, "")
+        s = s.replace("temp=", "")
+        temp = float(s)
+        print(temp)
+        client.publish("a_temp", temp)
 
